7105832d161dc3576ddd01d180a407dca350bc09
Ordan Gramatov <ordance01@yahoo.com>
2022-05-25 15:19:34 +0200

    Removed the button to add note when the user is not on the dashboard page

--- src/components/ContianerHeader/ContainerHeader.js
+++ src/components/ContianerHeader/ContainerHeader.js
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import React from 'react'
 import { RiMenuLine, RiEditBoxLine, RiCloseLine } from 'react-icons/ri';
 import { v4 as uuidv4 } from 'uuid';
 import moment from 'moment';
@@ -12,34 +13,36 @@ const ContainerHeader = () => {
 
   const toggleDrawer = () => {
     setMenuDrawerOpened(!menuDrawerOpened);
-  }
+  };
 
   return (
     <div className="container-header">
       <IconHolder
-          tooltip={menuDrawerOpened ? 'Close Menu' : 'Menu'}
-          onClick={toggleDrawer}
-          reactIcon={menuDrawerOpened ? <RiCloseLine /> : <RiMenuLine />}
-        />
-      <h3>Noteify</h3>
-      <IconHolder
-        tooltip="New Note"
-        onClick={() => {
-          const note = {
-            noteContent: {},
-            displayTitle: '',
-            dispayBody: '',
-            wordsToQuery: [],
-            id: uuidv4(),
-            createdAt: moment(),
-            selected: true
-          };
-          
-          addNote({ type: 'ADD_NOTE', payload: note });
-          setActiveNote(note);
-        }}
-        reactIcon={<RiEditBoxLine />}
+        tooltip={menuDrawerOpened ? 'Close Menu' : 'Menu'}
+        onClick={toggleDrawer}
+        reactIcon={menuDrawerOpened ? <RiCloseLine /> : <RiMenuLine />}
       />
+      <h3>Noteify</h3>
+      <div style={{'visibility' : `${document.title.toLowerCase().includes('dashboard') ? 'visible' : 'hidden'}`}}>
+        <IconHolder
+          tooltip="New Note"
+          onClick={() => {
+            const note = {
+              noteContent: {},
+              displayTitle: '',
+              dispayBody: '',
+              wordsToQuery: [],
+              id: uuidv4(),
+              createdAt: moment(),
+              selected: true,
+            };
+
+            addNote({ type: 'ADD_NOTE', payload: note });
+            setActiveNote(note);
+          }}
+          reactIcon={<RiEditBoxLine />}
+        />
+      </div>
     </div>
   );
 };
